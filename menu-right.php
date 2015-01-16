bcb49578f083b014cd792291af717448391ebc6c
SunDoctor <sun-doctor@7masterov.ru>
2015-01-17 00:27:58 +0300

    Add link to text

--- menu-right.php
+++ menu-right.php
@@ -8,14 +8,12 @@ $(document).ready(function(){
 </script>
     <ul class="mline">
         <li><a href="index.php?cmd=welcome">Home</a></li>
-<?php if (App::mod()->get('text_id')!=''): ?>
-        <li>| <a href="index.php?cmd=text-print&id=<?php echo App::mod()->get('text_id'); ?>">Print</a></li>
-<?php endif; ?>
 <?php if (isset($_SESSION['login']) && $_SESSION['login']==ROOT_LOGIN): ?>
 <?php if (App::mod()->get('text_id')!=''): ?>
         <li>| <a href="index.php?cmd=text-edit&id=<?php echo App::mod()->get('text_id'); ?>">Edit</a></li>
         <li>| <a href="index.php?cmd=text-move&id=<?php echo App::mod()->get('text_id'); ?>">Move</a></li>
         <li>| <a class="delobj" href="index.php?cmd=text-del&id=<?php echo App::mod()->get('text_id'); ?>">Delete</a></li>
+        <li>| <b>URL:</b> <?php echo htmlspecialchars(sprintf('index.php?cmd=text&id=%s',App::mod()->get('text_id'))); ?></li>
 <?php endif; ?>
 <?php endif; ?>
     </ul>
